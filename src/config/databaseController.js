0b6e5381e2a464a13784e7525b37fb539dfe72fa
Alexander <ec3715421@gmail.com>
2024-11-15 22:13:08 -0600

    Conexión a base de datos

---
+++ src/config/databaseController.js
@@ -0,0 +1,44 @@
+const mysql = require('mysql2');
+const { promisify } = require('util');
+const { database } = require('./keys');
+const { CONSTANTS } = require('../utils/utils');
+const { error } = require('console');
+
+const pool = mysql.createPool(database);
+
+pool.getConnection((error, conexion) => {
+
+    if(error){
+        switch(error.code){
+            case CONSTANTS.PROTOCOL_CONNECTION_LOST:
+                console.error('DATABASE CONNECTION WAS CLOSED');
+                break;
+
+            case CONSTANTS.ER_CON_COUNT_ERROR:
+                console.error('DATABASE HAS TO MANY CONNECTIONS');
+                break;
+
+            case CONSTANTS.ECONNREFUSED:
+                console.error('ACCESS DENIED FOR USER');
+                break;
+
+            default:
+                console.error('Error de base de datos no encontrado');
+                break;
+            
+        }
+    }
+
+
+    if(conexion){
+        console.log('Conexion establecida con la base de datos');
+        conexion.release();
+    }
+
+    return;
+
+});
+
+pool.query = promisify(pool.query);
+
+module.exports = pool;
---
+++ src/repositories/EstudianteRepository.js
@@ -0,0 +1,14 @@
+const pool = require('../config/databaseController');
+
+module.exports = {
+
+    obtenerTodosLosEstudiantes: async() => {
+        try{
+            const result = await pool.query('SELECT * FROM estudiantes');
+            return result;
+        }catch(error){
+            console.error('Ocurrio un problema al consultar la lista de estudiantes: ', error);
+        }
+    }
+
+}
